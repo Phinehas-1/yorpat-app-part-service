19a75cee23d2a4c5946fc53404f6e943b1b83e40
Phinehas Enakireru <phinehasenakireru@gmail.com>
2022-10-21 20:18:28 +0100

    modify datasource config, use bean method config for datasource instead of application.properties

---
+++ src/main/java/dev/bigdecimal/yorpat/api/partservice/PartServiceApplication.java
@@ -0,0 +1,28 @@
+package dev.bigdecimal.yorpat.api.partservice;
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class PartServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartServiceApplication.class, args);
+	}
+
+	@Bean
+	public DataSource dataSource() {
+		return DataSourceBuilder.create()
+				.driverClassName(System.getenv("DATABASE_DRIVER_CLASS"))
+				.url(System.getenv("JDBC_URL"))
+				.username(System.getenv("DATABASE_USER"))
+				.password(System.getenv("DATABASE_PASSWORD"))
+				.build();
+	}
+}
+
+}
---
+++ src/main/java/dev/bigdecimal/yorpat/api/partservice/ProgramModel.java
@@ -0,0 +1,10 @@
+package dev.bigdecimal.yorpat.api.partservice;
+
+import lombok.Data;
+
+@Data
+public class ProgramModel {
+    private Long programId;
+    private String programName;
+    private String programDate;
+}
